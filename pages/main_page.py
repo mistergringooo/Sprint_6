9985a54fa1321cda90af61af8c63c5e1060bbf67
Anton Skryabin <gring0frazy@yandex.ru>
2026-06-08 18:29:33 +0300

    Move answer texts to test_data module

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 from locators.main_page_locators import MainPageLocators
 from urls import DZEN_DOMAIN
@@ -8,72 +6,67 @@ import allure
 class MainPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        
+
+    @allure.step('Проверить переход на Дзен по логотипу Яндекса')
+    def check_yandex_logo(self):
+        self.switch_to_new_window(DZEN_DOMAIN)
+        return self.get_current_url()
+
+    @allure.step('Проверить переход на главную по логотипу Самоката')
+    def check_scooter_logo(self):
+        return self.get_current_url()
+
+    @allure.step('Клик по логотипу Яндекс')
+    def click_logo_yandex(self):
+        self.find_element(MainPageLocators.LOGO_YANDEX).click()
+
+    @allure.step('Клик по логотипу самоката')
+    def click_logo_scooter(self):
+        self.find_element(MainPageLocators.LOGO_SCOOTER).click()
+
     @allure.step('Получить элемент вопроса')
     def get_question(self, number):
-        locator = (MainPageLocators.QUESTION[0], MainPageLocators.QUESTION[1].format(number))
-        return self.driver.find_element(*locator)
-    
+        by, selector = MainPageLocators.QUESTION
+        locator = (by, selector.format(number))
+        return self.find_element(locator)
+
     @allure.step('Получить элемент ответа')
     def get_answer(self, number):
-        locator = (MainPageLocators.ANSWER[0], MainPageLocators.ANSWER[1].format(number))
-        return self.driver.find_element(*locator)
-    
+        by, selector = MainPageLocators.ANSWER
+        locator = (by, selector.format(number))
+        return self.find_element(locator)
+
     @allure.step('Получить текст ответа')
     def get_answer_text(self, number):
-        locator = (MainPageLocators.ANSWER[0], MainPageLocators.ANSWER[1].format(number))
-        return WebDriverWait(self.driver, 3).until(
-            EC.visibility_of_element_located(locator)
-        ).text
-    
+        by, selector = MainPageLocators.ANSWER
+        locator = (by, selector.format(number))
+        return self.wait_for_element(locator).text
+
     @allure.step('Клик по вопросу')
     def click_question(self, number):
-        locator = (MainPageLocators.QUESTION[0], MainPageLocators.QUESTION[1].format(number))
-        element = WebDriverWait(self.driver, 3).until(
-            EC.presence_of_element_located(locator)
-        )
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
-        self.driver.execute_script("arguments[0].click();", element)
+        by, selector = MainPageLocators.QUESTION
+        locator = (by, selector.format(number))
+        element = self.wait_for_element(locator)
+        self.scroll_to_element(element)
+        self.js_click(element)
 
     @allure.step('Проверка видимости ответа')
     def check_answer_visible(self, number):
-        locator = (MainPageLocators.ANSWER[0], MainPageLocators.ANSWER[1].format(number))
-        return WebDriverWait(self.driver, 3).until(
-            EC.visibility_of_element_located(locator)
-        )
-    
+        by, selector = MainPageLocators.ANSWER
+        locator = (by, selector.format(number))
+        return self.wait_for_element(locator)
+
     @allure.step('Клик по кнопке заказа сверху')
     def click_order_top(self):
-        self.driver.find_element(*MainPageLocators.ORDER_BUTTON_TOP).click()
+        self.find_element(MainPageLocators.ORDER_BUTTON_TOP).click()
 
     @allure.step('Клик по кнопке заказ внизу')
     def click_order_bottom(self):
-        self.driver.find_element(*MainPageLocators.ORDER_BUTTON_BOTTOM).click()
+        self.find_element(MainPageLocators.ORDER_BUTTON_BOTTOM).click()
 
     @allure.step('Принятие куков')
     def accept_cookies(self):
         try:
-            WebDriverWait(self.driver, 3).until(
-                EC.element_to_be_clickable(MainPageLocators.COOKIES)
-            ).click()
+            self.wait_for_clickable(MainPageLocators.COOKIES).click()
         except:
             pass
-    
-    @allure.step('Клик по логотипу самоката')
-    def click_logo_scooter(self):
-        self.driver.find_element(*MainPageLocators.LOGO_SCOOTER).click()
-
-    @allure.step('Клик по логотипу Яндекс')
-    def click_logo_yandex(self):
-        self.driver.find_element(*MainPageLocators.LOGO_YANDEX).click()
-
-    @allure.step('Проверить переход на Дзен по логотипу Яндекса')
-    def check_yandex_logo(self):
-        WebDriverWait(self.driver, 5).until(lambda d: len(d.window_handles) > 1)
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        WebDriverWait(self.driver, 10).until(lambda d: DZEN_DOMAIN in d.current_url)
-        return self.driver.current_url
-    
-    @allure.step('Проверить переход на главную по логотипу Самоката')
-    def check_scooter_logo(self):
-        return self.driver.current_url
--- pages/order_page.py
+++ pages/order_page.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from pages.base_page import BasePage
 from locators.order_page_locators import OrderPageLocators
@@ -11,60 +9,51 @@ class OrderPage(BasePage):
 
     @allure.step('Заполнить Имя')
     def set_name(self, name):
-        self.driver.find_element(*OrderPageLocators.NAME).send_keys(name)
+        self.find_element(OrderPageLocators.NAME).send_keys(name)
 
     @allure.step('Заполнить Фамилия')
     def set_surname(self, surname):
-        self.driver.find_element(*OrderPageLocators.SURNAME).send_keys(surname)
+        self.find_element(OrderPageLocators.SURNAME).send_keys(surname)
 
     @allure.step('Заполнить Адрес')
     def set_address(self, address):
-        self.driver.find_element(*OrderPageLocators.ADDRESS).send_keys(address)
+        self.find_element(OrderPageLocators.ADDRESS).send_keys(address)
 
     @allure.step('Заполнить Метро')
     def set_metro(self, metro):
-        self.driver.find_element(*OrderPageLocators.METRO_INPUT).send_keys(metro)
-        WebDriverWait(self.driver, 3).until(
-            EC.visibility_of_element_located(OrderPageLocators.METRO_OPTIONS_LIST)
-        )
-        self.driver.find_element(*OrderPageLocators.METRO_OPTION).click()
+        self.find_element(OrderPageLocators.METRO_INPUT).send_keys(metro)
+        self.wait_for_element(OrderPageLocators.METRO_OPTIONS_LIST)
+        self.find_element(OrderPageLocators.METRO_OPTION).click()
 
     @allure.step('Заполнить Телефон')
     def set_phone(self, phone):
-        self.driver.find_element(*OrderPageLocators.PHONE).send_keys(phone)
+        self.find_element(OrderPageLocators.PHONE).send_keys(phone)
 
     @allure.step('Клик по кнопке Далее')
     def click_next(self):
-        self.driver.find_element(*OrderPageLocators.NEXT).click()
+        self.find_element(OrderPageLocators.NEXT).click()
 
     @allure.step('Заполнить Дату')
     def set_date(self, date):
-        self.driver.find_element(*OrderPageLocators.DATE).send_keys(date)
-        self.driver.find_element(*OrderPageLocators.DATE).send_keys(Keys.ESCAPE)
+        self.find_element(OrderPageLocators.DATE).send_keys(date)
+        self.find_element(OrderPageLocators.DATE).send_keys(Keys.ESCAPE)
 
     @allure.step('Выбрать срок аренды')
     def set_rental_period(self, period):
-        WebDriverWait(self.driver, 3).until(
-            EC.element_to_be_clickable(OrderPageLocators.RENTAL_PERIOD)
-        ).click()
-        locator = (OrderPageLocators.RENTAL_PERIOD_OPTION[0], OrderPageLocators.RENTAL_PERIOD_OPTION[1].format(period))
-        WebDriverWait(self.driver, 3).until(
-            EC.visibility_of_element_located(locator)
-        ).click()
+        self.wait_for_clickable(OrderPageLocators.RENTAL_PERIOD).click()
+        by, selector = OrderPageLocators.RENTAL_PERIOD_OPTION
+        locator = (by, selector.format(period))
+        self.wait_for_element(locator).click()
 
     @allure.step('Клик по кнопке заказать')
     def click_order(self):
-        element = self.driver.find_element(*OrderPageLocators.CLICK_ORDER)
-        self.driver.execute_script("arguments[0].click();", element)
+        element = self.find_element(OrderPageLocators.CLICK_ORDER)
+        self.js_click(element)
 
     @allure.step('Клик по кнопке Да')
     def click_yes(self):
-        WebDriverWait(self.driver, 3).until(
-            EC.element_to_be_clickable(OrderPageLocators.CLICK_YES)
-        ).click()
+        self.wait_for_clickable(OrderPageLocators.CLICK_YES).click()
 
     @allure.step('Проверка подтверждения заказа')
     def check_order_success(self):
-        return WebDriverWait(self.driver, 3).until(
-            EC.visibility_of_element_located(OrderPageLocators.ORDER_SUCCESS)
-        )
+        return self.wait_for_element(OrderPageLocators.ORDER_SUCCESS)
